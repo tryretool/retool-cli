253dbf6452b84922f6fc508d5296ce86e427965e
AashJ <aashj99@gmail.com>
2023-07-10 13:58:14 -0700

    [feat] add scaffold command

--- src/commands/custom_component_cmds/scaffold.ts
+++ src/commands/custom_component_cmds/scaffold.ts
@@ -1,8 +1,21 @@
+import util from "util";
+import { exec as _exec } from "child_process";
+
+const exec = util.promisify(_exec);
+
 import { Argv } from "yargs";
+import ora from "ora";
 
 exports.command = "scaffold";
 exports.desc = "Scaffold a new custom component";
 exports.builder = {};
-exports.handler = function (argv: Argv) {
-  console.log("scaffolding a new custom component");
+exports.handler = async function (argv: Argv) {
+  const spinner = ora("Scaffolding a new custom component").start();
+  await exec(
+    "git clone https://github.com/tryretool/custom-component-guide.git"
+  );
+  spinner.stop();
+  console.log(
+    "Scaffolded a new custom component in the custom-component-guide directory."
+  );
 };
