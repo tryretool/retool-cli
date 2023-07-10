9314612767da82eb043b14d3aa0929f42e813b38
AashJ <aashj99@gmail.com>
2023-07-10 13:59:09 -0700

    [fix] clean up docs

--- src/commands/custom_component.ts
+++ src/commands/custom_component.ts
@@ -1,8 +1,7 @@
 import { Argv } from "yargs";
 
 exports.command = "custom-component <command>";
-exports.desc = "A set of commands to set up a custom component";
+exports.desc = "A set of commands for working with custom components.";
 exports.builder = function (yargs: Argv) {
   return yargs.commandDir("custom_component_cmds");
 };
-exports.handler = function (argv: Argv) {};
--- src/commands/custom_component_cmds/scaffold.ts
+++ src/commands/custom_component_cmds/scaffold.ts
@@ -3,13 +3,12 @@ import { exec as _exec } from "child_process";
 
 const exec = util.promisify(_exec);
 
-import { Argv } from "yargs";
 import ora from "ora";
 
 exports.command = "scaffold";
 exports.desc = "Scaffold a new custom component";
 exports.builder = {};
-exports.handler = async function (argv: Argv) {
+exports.handler = async function () {
   const spinner = ora("Scaffolding a new custom component").start();
   await exec(
     "git clone https://github.com/tryretool/custom-component-guide.git"
