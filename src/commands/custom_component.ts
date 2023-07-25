83a4aacf59edcc10acfa210a2b4eeb52b61b2fba
Peter Argany <petera@retool.com>
2023-07-25 09:27:23 -0700

    [fix] Fix custom-component command (remove subcommand😿)

--- src/commands/custom_component.ts
+++ src/commands/custom_component.ts
@@ -1,18 +1,37 @@
+import util from "util";
+import { exec as _exec } from "child_process";
+import ora from "ora";
 import { CommandModule } from "yargs";
 
-const command: CommandModule["command"] = "custom-component <command>";
-const describe: CommandModule["describe"] =
-  "A set of commands for working with custom components.";
-const builder: CommandModule["builder"] = function (yargs) {
-  return yargs.commandDir("custom_component_cmds");
+const exec = util.promisify(_exec);
+
+const command: CommandModule["command"] = "custom-component";
+const describe: CommandModule["describe"] = "Interface with custom components.";
+const builder: CommandModule["builder"] = {
+  clone: {
+    alias: "c",
+    describe: `Clones https://github.com/tryretool/custom-component-guide to the current directory.`,
+    demandOption: true,
+  },
+};
+const handler = async function (argv: any) {
+  if (argv.clone) {
+    const spinner = ora("Scaffolding a new custom component").start();
+    await exec(
+      "git clone https://github.com/tryretool/custom-component-guide.git"
+    );
+    spinner.stop();
+    console.log(
+      "Scaffolded a new custom component in the custom-component-guide directory."
+    );
+  }
 };
 
 const commandModule: CommandModule = {
   command,
   describe,
   builder,
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  handler: () => {},
+  handler,
 };
 
 export default commandModule;
