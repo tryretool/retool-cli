dfa9450ea7d49b6d0e2d7607096e452be8d77a90
Peter Argany <petera@retool.com>
2023-09-01 16:19:00 -0700

    [feat] Allow disabling telemetry

---
+++ src/commands/telemetry.ts
@@ -0,0 +1,46 @@
+import { CommandModule } from "yargs";
+
+import {
+  getAndVerifyCredentials,
+  persistCredentials,
+} from "../utils/credentials";
+
+const command = "telemetry";
+const describe = "Configure CLI telemetry.";
+const builder = {
+  disable: {
+    alias: "d",
+    describe: `Disable telemetry.`,
+  },
+  enable: {
+    alias: "e",
+    describe: `Enable telemetry.`,
+  },
+};
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const handler = function (argv: any) {
+  const credentials = getAndVerifyCredentials();
+
+  if (argv.disable) {
+    credentials.telemetryEnabled = false;
+    persistCredentials(credentials);
+    console.log("Successfully disabled telemetry. 📉");
+  } else if (argv.enable) {
+    credentials.telemetryEnabled = true;
+    persistCredentials(credentials);
+    console.log("Successfully enabled telemetry. 📈");
+  } else {
+    console.log(
+      "No flag specified. See `retool telemetry --help` for available flags."
+    );
+  }
+};
+
+const commandModule: CommandModule = {
+  command,
+  describe,
+  builder,
+  handler,
+};
+
+export default commandModule;
