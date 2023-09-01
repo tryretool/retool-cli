18aaf2f1c650b4dc54da0b151ec070123341937b
Mike Wang <mikewang.ca@berkeley.edu>
2023-09-01 10:08:48 -0700

    improve rpc client experience (#9)
    - create rpc resource for user
    - download all github files with tar approach to reduce number of api calls
    - improve steps to create resource:
    https://docs.google.com/document/d/1WA0dfBbbH5IkuZJWQqPri_mLfchbyLir4znkdVCsxx8/edit

--- src/utils/fileSave.ts
+++ src/utils/fileSave.ts
@@ -1,38 +1,54 @@
 import * as fs from "fs";
 import * as path from "path";
 
-import { getRequest } from "./networking";
+import * as tar from "tar";
 
 const axios = require("axios");
+const inquirer = require("inquirer");
 
-// url should be a link to a GitHub folder, e.g. https://api.github.com/repos/tryretool/retool-cli/contents/src/templates
-export async function downloadGithubFolder(url: string, directoryPath: string) {
+// url should be a tarfile link like https://api.github.com/repos/tryretool/retool-examples/tarball/main
+// subfolderPath should be a path to a subfolder within the tarfile, e.g. hello_world/typescript
+// destinationPath should be a path to a folder where the contents of the github subfolder will be extracted
+export async function downloadGithubSubfolder(
+  githubUrl: string,
+  subfolderPath: string,
+  destinationPath: string
+) {
   try {
-    // Fetch the directory content using GitHub API
-    const response = await getRequest(url);
-    const contents = response.data;
+    const url = `${githubUrl}?path=${subfolderPath}`;
+    const response = await axios.get(url, { responseType: "arraybuffer" });
 
-    // Create the save directory if it doesn't exist
-    if (!fs.existsSync(directoryPath)) {
-      fs.mkdirSync(directoryPath, { recursive: true });
-    }
-
-    // Iterate through the files and directories and download/save each
-    for (const item of contents) {
-      const itemName = item.name;
-      const itemPath = path.join(directoryPath, itemName);
-
-      if (item.type === "file") {
-        const fileResponse = await axios.get(item.download_url, {
-          responseType: "arraybuffer",
-        });
-        fs.writeFileSync(itemPath, fileResponse.data);
-      } else if (item.type === "dir") {
-        await downloadGithubFolder(`${url}/${itemName}`, itemPath);
+    // Delete the directory if it already exists
+    if (fs.existsSync(destinationPath)) {
+      const { proceedWithDirectoryCreation } = (await inquirer.prompt([
+        {
+          name: "proceedWithDirectoryCreation",
+          message:
+            "It looks like this directory already exists, can we delete it and continue? (Y/N)",
+          type: "input",
+        },
+      ])) as { proceedWithDirectoryCreation: string };
+      if (proceedWithDirectoryCreation.toLowerCase() !== "y") {
+        console.log("Aborting...");
+        process.exit(1);
       }
+      await fs.promises.rm(destinationPath, { recursive: true });
     }
+
+    fs.mkdirSync(destinationPath, { recursive: true });
+
+    const tarballPath = path.join(destinationPath, "tarball.tar.gz");
+    fs.writeFileSync(tarballPath, response.data);
+
+    await tar.x({
+      file: tarballPath,
+      cwd: destinationPath,
+      strip: subfolderPath.split("/").length + 1, // remove the top-level directories
+    });
+
+    fs.unlinkSync(tarballPath);
   } catch (error: any) {
-    console.error("Error downloading files:", error.message);
+    console.error("Error:", error.message);
   }
 }
 
